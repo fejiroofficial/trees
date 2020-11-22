b78d84e8290abc9b2cb480ed9a707e8dcaa46acc
fejiroofficial <fejiro.gospel@gmail.com>
2020-11-22 15:10:44 +0100

    side nav component

--- src/App.vue
+++ src/App.vue
@@ -1,43 +1,16 @@
 <template>
   <div id="app">
-    <div class="burger-menu" type="primary" @click="showDrawer">
-      <div class="burger-menu__line"></div>
-      <div class="burger-menu__line burger-menu__line_mid"></div>
-      <div class="burger-menu__line"></div>
-    </div>
-    <a-drawer
-      placement="right"
-      :closable="false"
-      :visible="visible"
-      @close="onClose"
-    >
-      <div>
-        <router-link to="/">Trees</router-link>
-      </div>
-      <a-divider type="vertical" />
-      <div>
-        <router-link to="/gifs">Gifs</router-link>
-      </div>
-    </a-drawer>
+    <SideNav />
     <router-view></router-view>
   </div>
 </template>
 
 <script>
+import SideNav from "@/components/SideNav";
 export default {
   name: "App",
-  data() {
-    return {
-      visible: false,
-    };
-  },
-  methods: {
-    showDrawer() {
-      this.visible = true;
-    },
-    onClose() {
-      this.visible = false;
-    },
+  components: {
+    SideNav,
   },
 };
 </script>
@@ -48,21 +21,5 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   color: #2c3e50;
-}
-.burger-menu {
-  cursor: pointer;
-  width: fit-content;
-  position: absolute;
-  top: 2%;
-  left: 2%;
-  &__line {
-    width: 30px;
-    height: 1px;
-    background-color: black;
-    margin: 6px 0;
-    &_mid {
-      width: 23px;
-    }
-  }
 }
 </style>
---
+++ src/components/SideNav.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="nav">
+    <div class="burger-menu" type="primary" @click="showDrawer">
+      <div class="burger-menu__line"></div>
+      <div class="burger-menu__line burger-menu__line_mid"></div>
+      <div class="burger-menu__line"></div>
+    </div>
+    <a-drawer
+      placement="right"
+      :closable="false"
+      :visible="visible"
+      @close="onClose"
+    >
+      <div>
+        <router-link to="/">Trees</router-link>
+      </div>
+      <a-divider type="vertical" />
+      <div>
+        <router-link to="/gifs">Gifs</router-link>
+      </div>
+    </a-drawer>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      visible: false,
+    };
+  },
+  methods: {
+    showDrawer() {
+      this.visible = true;
+    },
+    onClose() {
+      this.visible = false;
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.burger-menu {
+  cursor: pointer;
+  width: fit-content;
+  position: absolute;
+  top: 2%;
+  left: 2%;
+  &__line {
+    width: 30px;
+    height: 1px;
+    background-color: black;
+    margin: 6px 0;
+    &_mid {
+      width: 23px;
+    }
+  }
+}
+</style>
